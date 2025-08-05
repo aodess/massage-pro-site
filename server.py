2a9f0e8b64119f01dc9ad4f2ee9713a28d05326b
Ecvgeniy <chenkoeniy@gmail.com>
2025-08-05 13:28:28 +0300

    Initial commit - Massage Workshop site

--- server.py
+++
@@ -1,7 +0,0 @@
-from http.server import HTTPServer, SimpleHTTPRequestHandler
-import os
-
-port = int(os.environ.get('PORT', 8080))
-httpd = HTTPServer(('', port), SimpleHTTPRequestHandler)
-print(f'Server running on port {port}')
-httpd.serve_forever()
